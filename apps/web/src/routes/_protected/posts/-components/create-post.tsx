b4d4c415e8c93c6511e4a6b206c12ab168413a58
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-21 09:18:23 +1100

    fix(web): sliding animation for create post dialog

--- apps/web/src/routes/_protected/posts/-components/create-post.tsx
+++ apps/web/src/routes/_protected/posts/-components/create-post.tsx
@@ -84,7 +84,7 @@ and to the unceasing vigilance of agents of the United States Handicapper Genera
           Create
         </Button>
       </DialogTrigger>
-      <DialogContent className="max-w-[90vw] xl:max-w-screen-lg data-[state=open]:slide-in-from-right data-[state=closed]:slide-out-to-right">
+      <DialogContent className="max-w-[90vw] xl:max-w-screen-lg data-[state=open]:slide-in-from-right-1/3 data-[state=closed]:slide-out-to-right-1/3">
         <DialogHeader>
           <DialogTitle>Create Post</DialogTitle>
           <DialogDescription>
