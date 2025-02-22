b218badffad43db737a635a6ea7005bb1cc30a80
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-22 22:49:27 +1100

    docs(web): mentions PUBLIC_SERVER_URL should be passed as build arg

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -3,7 +3,10 @@ import * as v from 'valibot';
 export const CLIENT_ENV_PREFIX = 'PUBLIC_' as const;
 
 export const envSchema = v.object({
-  // This is the backend API server
+  /**
+   * This is the backend API server. Note that this should
+   * be passed as a build-time variable (ARG) in docker.
+   */
   PUBLIC_SERVER_URL: v.pipe(v.string(), v.url()),
 });
 
