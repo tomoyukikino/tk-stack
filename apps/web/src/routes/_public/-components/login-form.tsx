65c20aae43f06dcbe046822f36d4ac021d4a2524
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-02-23 12:39:20 +1100

    chore(web): remove rememberMe field

--- apps/web/src/routes/_public/-components/login-form.tsx
+++ apps/web/src/routes/_public/-components/login-form.tsx
@@ -34,7 +34,6 @@ export default function LoginCredentialsForm() {
         {
           email: value.email,
           password: value.password,
-          rememberMe: false,
         },
         {
           onSuccess: () => {
