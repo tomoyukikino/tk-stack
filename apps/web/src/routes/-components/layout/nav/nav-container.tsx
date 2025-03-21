3e5529404a51187416a4f9ef428a2d71218eb3aa
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-03-21 16:10:16 +1100

    fix(web): readonly children for nav container

--- apps/web/src/routes/-components/layout/nav/nav-container.tsx
+++ apps/web/src/routes/-components/layout/nav/nav-container.tsx
@@ -1,6 +1,10 @@
-import type { ReactNode } from '@tanstack/react-router';
+import type { ReactNode } from 'react';
 
-export default function NavContainer({ children }: { children?: ReactNode }) {
+export default function NavContainer({
+  children,
+}: {
+  children?: Readonly<ReactNode>;
+}) {
   return (
     <div className="px-2 md:px-4 flex items-center justify-between text-lg bg-nav h-12">
       {children}
