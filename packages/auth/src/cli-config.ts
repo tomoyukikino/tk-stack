70b75945820772262a30cff5e909bd5bb928cb56
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-03-23 01:22:17 +1100

    feat(auth,db): re-generate schema + added docs for better-auth CLI

--- packages/auth/src/cli-config.ts
+++ packages/auth/src/cli-config.ts
@@ -2,14 +2,19 @@ import { createDb } from '@repo/db/client';
 import { createAuth } from './server';
 
 /**
+ * @internal
+ *
  * This export is needed strictly for the CLI to work with
  *     pnpm auth:schema:generate
  *
  * It should not be imported or used for any other purpose.
+ *
+ * The documentation for better-auth CLI can be found here:
+ * - https://www.better-auth.com/docs/concepts/cli
  */
 export const auth = createAuth({
-  webUrl: 'https://unused.example.value',
-  authSecret: 'secret',
+  webUrl: 'https://placeholder.com',
+  authSecret: '',
   db: createDb({
     databaseUrl: '',
   }),
